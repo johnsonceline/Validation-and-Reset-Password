06240f9cd2ea5d17b44fafd6c64374e60420c783
Celine Johnson <johnsonceline670gmail.com>
2019-08-06 04:33:27 -0500

    Part 1 validation cms

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  {
+    path: 'home',
+    loadChildren: './home/home.module#HomePageModule',
+    
+  },
+  
+  { path: 'reset-password', loadChildren: './pages/reset-password/reset-password.module#ResetPasswordPageModule' },
+  { path: 'signup', loadChildren: './pages/signup/signup.module#SignupPageModule' },
+  { path: 'profile', loadChildren: './pages/profile/profile.module#ProfilePageModule' },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/credentials.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCNkOqo3GECKFGbhq3oLTmlc163vRyC-20",
+    authDomain: "hotelapp-b6b6e.firebaseapp.com",
+    databaseURL: "https://hotelapp-b6b6e.firebaseio.com",
+    projectId: "hotelapp-b6b6e",
+    storageBucket: "hotelapp-b6b6e.appspot.com",
+    messagingSenderId: "617518464883",
+    appId: "1:617518464883:web:2248661b3e234446"
+  };
